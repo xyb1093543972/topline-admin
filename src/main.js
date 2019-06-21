81ba12bd64020edd29adc2d7509e41f0aa246806
xyb1093543972 <xyb1093543972@163.com>
2019-06-21 15:46:32 +0800

    ooooo

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-
+import './styles/index.less'
 Vue.use(ElementUI)
+Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
   router
---
+++ src/views/login/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class='appLogin'>
+    <el-form :label-position='labelPosition' label-width='80px' :model='formLabelAlign'>
+      <el-form-item label='名称'>
+        <el-input v-model='formLabelAlign.name'></el-input>
+      </el-form-item>
+      <el-form-item label='活动区域'>
+        <el-input v-model='formLabelAlign.region'></el-input>
+      </el-form-item>
+      <el-form-item label='活动形式'>
+        <el-input v-model='formLabelAlign.type'></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
