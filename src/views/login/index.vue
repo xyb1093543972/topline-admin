5cde93226cd27681df565ad78e86b3edd6f4165e
xyb1093543972 <xyb1093543972@163.com>
2019-06-21 16:42:22 +0800

    gt

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,28 +1,57 @@
 <template>
-  <div class='appLogin'>
-    <el-form :label-position='labelPosition' label-width='80px' :model='formLabelAlign'>
-      <el-form-item label='名称'>
-        <el-input v-model='formLabelAlign.name'></el-input>
-      </el-form-item>
-      <el-form-item label='活动区域'>
-        <el-input v-model='formLabelAlign.region'></el-input>
-      </el-form-item>
-      <el-form-item label='活动形式'>
-        <el-input v-model='formLabelAlign.type'></el-input>
-      </el-form-item>
-    </el-form>
+  <div class="appLogin">
+    <!-- 给组件加 class，会把这个 class 作用到组件的根元素上 -->
+    <div class="form-wrap">
+      <div class="form-head">
+        <img src="./logo_index.png" alt="黑马头条号">
+      </div>
+      <el-form class="form-content" ref="form">
+        <el-form-item>
+          <el-input placeholder="手机号" v-model="loginForm.mobile"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <!-- el-col 栅格布局，一共 24 列，:span 用来指定占用的大小，:offset 用来指定偏移量 -->
+          <el-col :span="14">
+            <el-input placeholder="验证码" v-model="loginForm.code"></el-input>
+          </el-col>
+          <el-col :offset="1" :span="9">
+            <el-button @click="getCode">获取验证码</el-button>
+          </el-col>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="btn-login" type="primary">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import '@/vendor/gt'
 export default {
   data() {
     return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    getCode() {
+      axios({})
     }
   }
 }
 </script>
 
 <style lang='less' scoped>
-
+.appLogin {
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  background: url("./1.jpg") center;
+}
 </style>
